5e4b9f3a01c26800ae34a882e6b52ebd629fd3e6
kauanconstancio <kauanconstancio13@gmail.com>
2025-05-22 07:09:07 -0300

    finished

---
+++ main.js
@@ -0,0 +1,72 @@
+let dados = [
+    {
+        nome: "Se soubesse",
+        link: "https://www.instagram.com/reel/DGDEdiBxY-0/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA==",
+    },
+    {
+        nome: "Nao sei ao certo",
+        link: "https://www.instagram.com/reel/DHr7c-OgVKD/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA==",
+    },
+    {
+        nome: "Saudade",
+        link: "https://www.instagram.com/reel/DGUnFUbusHT/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA==",
+    },
+    {
+        nome: "Mochi..",
+        link: "https://www.instagram.com/reel/DHtPgKgOQaJ/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA==",
+    },
+    {
+        nome: "Para Sempre",
+        link: "https://www.instagram.com/reel/DIkI5QKSd8A/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA==",
+    },
+    {
+        nome: "Seus Olhos",
+        link: "https://www.instagram.com/reel/DJNKOMXSWtE/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA==",
+    },
+    {
+        nome: "Para Sempre",
+        link: "https://www.instagram.com/reel/DI-GLZVRgHn/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA==",
+    },
+    {
+        nome: "Sim...",
+        link: "https://www.instagram.com/reel/DFokkUSAUht/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA==",
+    },
+    {
+        nome: "Eu Prometi..",
+        link: "https://www.instagram.com/reel/DH25vCpurmE/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA==",
+    },
+    {
+        nome: "Eu te Amo",
+        link: "https://www.instagram.com/reel/DHuM11bPXQ3/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA==",
+    },
+    {
+        nome: "Sou Grato",
+        link: "https://www.instagram.com/reel/DIPsCggSRZh/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA==",
+    },
+    {
+        nome: "Tudo em Voce..",
+        link: "https://www.instagram.com/reel/DB7pvATAmgU/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA==",
+    },
+    {
+        nome: "Sei Exatamente",
+        link: "https://www.instagram.com/reel/DGtLm1pv-7G/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA==",
+    },
+    {
+        nome: "Ansiedade",
+        link: "https://www.instagram.com/reel/DHe2L5Bx7zC/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA==",
+    },
+    {
+        nome: "Nao da..",
+        link: "https://www.instagram.com/reel/DGJlwMURcIK/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA==",
+    },
+]
+
+let linksButtons = document.getElementById('linksButtons');
+
+for (let video of dados) {
+    linksButtons.innerHTML += `
+    <div class="link-button">
+        <a href="${video.link}">${video.nome}</a>
+    </div>
+    `
+}
